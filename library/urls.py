bd33f51c9a62c85eae09b783dff81be2272b2333
lonslaught <lonslaught.arch@gmail.com>
2024-08-18 16:53:27 +0500

    Реализация контроллеров CBV для Books

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import books_list, books_detail
+from .views import BooksListView, BookCreateView, BookUpdateView, BookDeleteView, BookDetailView
 
 app_name = 'library'
 
 urlpatterns = [
-    path('books_list/', books_list, name='books_list'),
-    path('book_detail/<int:book_id>', books_detail, name='book_detail'),
+    path('books/', BooksListView.as_view(), name='books_list'),
+    path('books/new/', BookCreateView.as_view(), name='book_create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book_update'),
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book_delete'),
 ]
